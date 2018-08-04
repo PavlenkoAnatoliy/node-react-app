70b9208ed01fe489600b35d421fdd29d49fa1027
Tolik <Tolik@MacBook-Pro-Tolik.local>
2018-08-04 20:25:28 +0300

    Prepare for redux-form

---
+++ client/src/components/SurveyNew.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+
+}
+
+export default SurveyNew;
--- routes/surveyRouts.js
+++ routes/surveyRouts.js
@@ -3,9 +3,15 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
 const Survey = mongoose.model('surveys');
+const Mailer = require('../services/mailer');
+const surveyTemplates = require('../services/emailTemplates/surveyTemplates');
 
 module.exports = (app) => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('THanks for woing')
+  })
+
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const {
       title,
       subject,
@@ -20,6 +26,19 @@ module.exports = (app) => {
       recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now(),
-    })
+    });
+
+    const mailer = new Mailer(survey, surveyTemplates(survey));
+    
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 }
