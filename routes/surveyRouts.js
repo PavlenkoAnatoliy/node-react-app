1c144aa8895074922075c1f1db4f0d4bd445d0bf
Tolik <Tolik@MacBook-Pro-Tolik.local>
2018-08-05 23:31:22 +0300

    Update surveyRoutes algorithm

--- routes/surveyRouts.js
+++ routes/surveyRouts.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -5,11 +8,30 @@ const requireCredits = require('../middlewares/requireCredits');
 const Survey = mongoose.model('surveys');
 const Mailer = require('../services/mailer');
 const surveyTemplates = require('../services/emailTemplates/surveyTemplates');
+const keys = require('./../config/keys');
 
 module.exports = (app) => {
   app.get('/api/surveys/thanks', (req, res) => {
     res.send('THanks for woing')
-  })
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    const events = _.chain(req.body)
+      .map( ({ url, email }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
+    console.log(events);
+    
+    // res.send({});
+  });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const {
@@ -28,7 +50,7 @@ module.exports = (app) => {
       dateSent: Date.now(),
     });
 
-    const mailer = new Mailer(survey, surveyTemplates(survey, { redirectDomain: '/' }));
+    const mailer = new Mailer(survey, surveyTemplates(survey, keys));
     
     try {
       await mailer.send();
